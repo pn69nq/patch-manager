7ae50554bc38670e1886e2a8038d5141dfb872e6
zengqiang <ihelpgodness@sina.com>
2017-11-29 13:45:59 +0800

    添加辅助方法

---
+++ api/util/Md5Util.py
@@ -0,0 +1,9 @@
+import hashlib
+
+# 对文件进行md5 计算
+def CalcFileMD5(filepath):
+    with open(filepath,'rb') as f:
+        md5obj = hashlib.md5()
+        md5obj.update(f.read())
+        hash = md5obj.hexdigest()
+        return hash
